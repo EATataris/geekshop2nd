5b642df0e9783d9274045d323c383649ead41845
Sasuke <emir-ali818@mail.ru>
2021-09-09 21:44:57 +0300

    Added whole lesson3

--- products/views.py
+++ products/views.py
@@ -1,10 +1,6 @@
-import json
-import os
-
 from django.shortcuts import render
-
+from products.models import Product, ProductCategory
 # Create your views here.
-MODULE_DIR = os.path.dirname(__file__)
 
 
 def index(request):
@@ -14,11 +10,10 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 def products(request):
-    file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
     context = {
         'title': 'Geekshop - каталог',
-        'categories': ['Новинки', 'Одежда', 'Обувь', 'Аксессуары', 'Подарки'],
-        'products': json.load(open(file_path, encoding='utf-8'))
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all()
     }
 
     return render(request, 'products/products.html', context)

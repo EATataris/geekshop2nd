38f75ef3f8bf7f9e372ac2617dd2f7419d898635
Sasuke <emir-ali818@mail.ru>
2021-10-08 21:40:58 +0300

    add display profile page

--- users/views.py
+++ users/views.py
@@ -36,3 +36,7 @@ def registration(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def profile(request):
+    return render(request, 'users/profile.html')
